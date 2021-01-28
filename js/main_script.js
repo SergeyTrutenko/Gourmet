82496dabd7cccc6f620359e3ddeb1e1f2cb20ce7
SergeyTrutenko <seregatru94@gmail.com>
2021-01-28 17:08:01 +0200

    desktop version

--- js/main_script.js
+++ js/main_script.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
 
+    $('.main-screen__slider').slick({
+        infinite: true,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true
+    });
+
+    $('.product__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true
+    });
+
+    $('.recipes__slider').slick({
+        infinite: true,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: true,
+        prevArrow: '<button class="prev"><i class="fas fa-caret-left"></i></button>',
+        nextArrow: '<button class="next"><i class="fas fa-caret-right"></i></button>'
+    });
+
+    $('.find__slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true
+    });
+
 });
